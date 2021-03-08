f25ba7efe1554a97e014ce53c73c88696e87b884
ida03 <73817507+ida03@users.noreply.github.com>
2021-03-08 10:20:12 +0100

    Lagt till kod för Selectionsort + en stopwatch

--- Sorteringalgoritmer/Program.cs
+++ Sorteringalgoritmer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,24 @@ namespace Sorteringalgoritmer
                 }
             }
         }
+        static void Selectionsort(List<int> myList)
+        {
+            int temp, smallest;
+            for (int i=0; i<myList.Count-1; i++)
+            {
+                smallest = i;
+                for (int j = i+1; j<myList.Count; j++)
+                {
+                    if (myList[j] < myList[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = myList[smallest];
+                myList[smallest] = myList[i];
+                myList[i] = temp;
+            }
+        }
 
         static void SkapaSlumptallista(List<int>myList,int size)
         {
@@ -35,10 +54,25 @@ namespace Sorteringalgoritmer
 
         static void Main(string[] args)
         {
-            List<int> tallista = new List<int>();
-            SkapaSlumptallista(tallista, 10);
-            Bubblesort(tallista);
+            Stopwatch sw = new Stopwatch();
+            List<int> tallistaB = new List<int>();
+            List<int> tallistaS = new List<int>();
+
+            sw.Start();
+            SkapaSlumptallista(tallistaB, 100000);
+            Bubblesort(tallistaB);
+            sw.Stop();
             Console.WriteLine("Listan är sorterad!");
+            Console.WriteLine("Sorteringstid för Bubblesort " + sw.ElapsedMilliseconds);
+
+            sw.Reset();
+
+            sw.Start();
+            SkapaSlumptallista(tallistaS, 100000);
+            Selectionsort(tallistaS);
+            sw.Stop();
+            Console.WriteLine("Listan är sorterad!");
+            Console.WriteLine("Sorteringstid för Selectionsort " + sw.ElapsedMilliseconds);
         }
     }
 }
